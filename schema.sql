0da6aed2e1e5c695e83b06c2622c29388604af09
nimplay <nimrod7day@gmail.com>
2022-09-12 12:45:10 -0400

    adding data

--- schema.sql
+++ schema.sql
@@ -1,5 +1,17 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+CREATE TABLE IF NOT EXISTS public.animals
+(
+    id bigint NOT NULL,
+    name name COLLATE pg_catalog."C" NOT NULL,
+    date_of_birth date NOT NULL,
+    neutered boolean NOT NULL,
+    weight_kg numeric NOT NULL,
+    escape_attempts bigint,
+    CONSTRAINT animals_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.animals
+    OWNER to postgres;
